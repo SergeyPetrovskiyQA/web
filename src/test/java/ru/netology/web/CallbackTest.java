01fc9676e2d092a6bcbbb43e627dd4054c74e9f9
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-03-08 08:27:38 +0300

    add negative test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -17,6 +17,8 @@ class ChromeTest {
     static void setupAll() {
         options = new ChromeOptions();
         options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
         WebDriverManager.chromedriver().setup();
 
     }
@@ -59,25 +61,54 @@ class ChromeTest {
     @Test
     void negativeTestV1() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Петров Семён");
-        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79001231212");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Семёнов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79614402259");
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.cssSelector("[class=\"button__content\"]")).click();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.className("input__sub")).getText();
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                driver.findElement(By.cssSelector("[data-test-id=\"name\"].input_invalid .input__sub")).getText());
     }
 
     @Test
     void negativeTestV2() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Li Chang");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("");
         driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+7900123121");
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.cssSelector("[class=\"button__content\"]")).click();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.className("input__sub")).getText();
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=\"name\"].input_invalid .input__sub")).getText());
+    }
+    @Test
+    void negativeTestV3() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.cssSelector("[class=\"button__content\"]")).click();
+        Assertions.assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=\"phone\"].input_invalid .input__sub")).getText());
+    }
+
+    @Test
+    void negativeTestV4() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+790012312");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.cssSelector("[class=\"button__content\"]")).click();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                driver.findElement(By.cssSelector("[data-test-id=\"phone\"].input_invalid .input__sub")).getText());
+
+    }
+
+    @Test
+    void negativeTestV5() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79001231211");
+        driver.findElement(By.cssSelector("button.button")).click();
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).isDisplayed());
     }
 }
 
